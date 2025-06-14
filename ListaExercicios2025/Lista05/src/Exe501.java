b9ffb97149f55b0db1355067df66f3d65ed10da4
Tarcisio Jacobsen <cisojacobsen@gmail.com>
2025-06-14 15:52:50 -0300

    Exercicio 501 realizado

---
+++ ListaExercicios2025/Lista05/src/Exe501.java
@@ -0,0 +1,47 @@
+
+/**
+ * 1) Escreva um programa que leia o código de um determinado produto
+ *    e mostre a sua classificação. Utilize a tabela abaixo como referência.
+ * 
+ *    Código - Descrição
+ *    1 - Alimento não perecível
+ *    2 - Alimento perecível
+ *    3 - Vestuário
+ *    4 - Limpeza
+ */
+
+import java.util.Scanner;
+
+public class Exe501 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int codigo;
+
+        System.out.print("Categoria de Produto");
+        System.out.print("\n 1 - Alimento Não Perecível\r\n" + //
+                " 2 - Alimento Perecível\r\n" + //
+                " 3 - Vestuário\r\n" + //
+                " 4 - Limpeza");
+        System.out.print("\n\nInforme um dos códigos: ");
+        codigo = sc.nextInt();
+        sc.close();
+
+        switch (codigo) {
+            case 1:
+                System.out.print("Você selecionou a categoria Alimento Não Perecível");
+                break;
+            case 2:
+                System.out.print("Você selecionou a categoria Alimento Perecível");
+                break;
+            case 3:
+                System.out.print("Você selecionou a categoria Vestuário");
+                break;
+            case 4:
+                System.out.print("Você selecionou a categoria Limpeza");
+                break;
+            default:
+                System.out.print("Categoria não cadastrada!\nEncerrando programa.");
+                break;
+        }
+    }
+}
